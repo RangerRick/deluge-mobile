4c3d1f77c3564f04158357811a127f47c2a9b613
Benjamin Reed <rangerrick@befunk.com>
2014-04-30 23:31:54 -0400

    working icon for ios

---
+++ .cordova/hooks/after_prepare/01_images.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for FILE in images/icons/*.png; do
+	SHORTNAME=`basename $FILE`
+	case "$FILE" in
+		*-2x.png)
+			OUTFILE=`echo "$SHORTNAME" | sed -e 's,-2x.png,\@2x.png,g'`
+			cp "$FILE" "platforms/ios/DelugeMobile/Resources/icons/$OUTFILE"
+			;;
+		*)
+			cp "$FILE" "platforms/ios/DelugeMobile/Resources/icons/$SHORTNAME"
+			;;
+	esac
+done
+
+cp 'images/icons/icon-57.png'    'platforms/ios/DelugeMobile/Resources/icons/icon.png'
+cp 'images/icons/icon-57-2x.png' 'platforms/ios/DelugeMobile/Resources/icons/icon@2x.png'
