af4d3518ec3d4fb381d8d0298f014df0ec260acd
Benjamin Reed <rangerrick@befunk.com>
2014-04-30 23:35:02 -0400

    forgot these were referenced from index.html

--- .cordova/hooks/after_prepare/01_images.sh
+++ .cordova/hooks/after_prepare/01_images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for FILE in images/icons/*.png; do
+for FILE in www/res/icon/ios/*.png; do
 	SHORTNAME=`basename $FILE`
 	case "$FILE" in
 		*-2x.png)
@@ -13,5 +13,5 @@ for FILE in images/icons/*.png; do
 	esac
 done
 
-cp 'images/icons/icon-57.png'    'platforms/ios/DelugeMobile/Resources/icons/icon.png'
-cp 'images/icons/icon-57-2x.png' 'platforms/ios/DelugeMobile/Resources/icons/icon@2x.png'
+cp 'www/res/icon/ios/icon-57.png'    'platforms/ios/DelugeMobile/Resources/icons/icon.png'
+cp 'www/res/icon/ios/icon-57-2x.png' 'platforms/ios/DelugeMobile/Resources/icons/icon@2x.png'
