16b399329b974a8bbd863b2cdbfcf0bfa2a44125
Benjamin Reed <rangerrick@befunk.com>
2014-05-01 23:12:38 -0400

    plugin install on platform add now

---
+++ .cordova/hooks/after_platform_add/01_plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cordova plugin add 'com.verso.cordova.clipboard'
+cordova plugin add 'org.apache.cordova.console'
+cordova plugin add 'org.apache.cordova.device'
+cordova plugin add 'org.apache.cordova.statusbar'
--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ sudo npm -g install bower ionic
 bower install
 ionic platform ios android
 
-cordova plugin add 'com.verso.cordova.clipboard'
-cordova plugin add 'org.apache.cordova.console'
-cordova plugin add 'org.apache.cordova.device'
-cordova plugin add 'org.apache.cordova.statusbar'
+# this is done with a hook now
+#cordova plugin add 'com.verso.cordova.clipboard'
+#cordova plugin add 'org.apache.cordova.console'
+#cordova plugin add 'org.apache.cordova.device'
+#cordova plugin add 'org.apache.cordova.statusbar'
