80ca36cfd88264fe536a1646932006d4cd0f6fcc
Benjamin Reed <rangerrick@befunk.com>
2014-05-01 23:32:39 -0400

    generate versionCode

--- .cordova/hooks/after_prepare/02_version.sh
+++ .cordova/hooks/after_prepare/02_version.sh
@@ -5,3 +5,11 @@ NEWVERSION=`cat "$ROOT/www/config.xml" | grep '<widget' | sed -e 's,^.*version="
 echo "=== Setting version to $NEWVERSION ==="
 sed -e "s,@VERSION@,$NEWVERSION,g" "$ROOT/www/templates/settings.html" > "$ROOT/platforms/android/assets/www/templates/settings.html"
 sed -e "s,@VERSION@,$NEWVERSION,g" "$ROOT/www/templates/settings.html" > "$ROOT/platforms/ios/www/templates/settings.html"
+
+MAJOR=`echo $NEWVERSION | cut -d. -f1`
+MINOR=`echo $NEWVERSION | cut -d. -f2`
+MICRO=`echo $NEWVERSION | cut -d. -f3`
+
+VERSIONCODE=`printf '%d%02d%02d' "$MAJOR" "$MINOR" "$MICRO"`
+
+perl -pi -e "s,android:versionCode=\"\d+\",android:versionCode=\"$VERSIONCODE\",g" 'platforms/android/AndroidManifest.xml'
